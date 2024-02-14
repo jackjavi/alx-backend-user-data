7ca1d435a02a67c859e80ac561dd24f2c5edd8d9
jackjavi <jackmtembete@gmail.com>
2024-02-14 05:25:24 +0300

    Unauthorized.

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,6 +21,15 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """
+    Error handler: Unauthorized
+    HTTP status code for an authorized request
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
