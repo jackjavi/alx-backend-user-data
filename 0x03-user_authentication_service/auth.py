2eafbf78cd2482cdaa93e994f4c0181b83cee55d
jackjavi <jackmtembete@gmail.com>
2024-02-25 13:45:54 +0300

    Credentials Validation.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -38,3 +38,19 @@ class Auth:
 
         else:
             raise ValueError(f'User {email} already exists')
+        
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Checks if a user's login details are valid.
+        """
+        user = None
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                return bcrypt.checkpw(
+                    password.encode("utf-8"),
+                    user.hashed_password,
+                )
+        except NoResultFound:
+            return False
+        return False
